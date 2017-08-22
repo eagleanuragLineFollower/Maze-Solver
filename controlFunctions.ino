333deb041c5ad2248979b3dcc08a61d72bd587b2
Kenny Chan <chankenny900@gmail.com>
2017-08-21 21:53:14 -0700

    Add files via upload

---
+++ controlFunctions.ino
@@ -0,0 +1,132 @@
+void frontCrashTest()
+{
+  
+  int currDist = analogRead(FRONTSENSOR) - baseLevelF;
+  //Serial.println(currDist);
+  if (currDist > 200)
+  {
+     //Serial.println("Front alert");
+     analogWrite(PWM1, 0);
+     analogWrite(PWM2, 0);
+     //delay(2000);
+     if ((analogRead(RIGHTSENSOR) - analogRead(LEFTSENSOR)) > 300)
+     {
+      //Serial.println("Turning left");
+      turn90(1);
+     }
+     else if ((analogRead(RIGHTSENSOR) - analogRead(LEFTSENSOR)) < -300)
+     {
+      //Serial.println("Turning right");
+      turn90(0);
+     }
+     else if (abs(analogRead(RIGHTSENSOR) - analogRead(LEFTSENSOR)) < 40)
+     {
+        lMSpeed =0;
+        rMSpeed =0;
+        while(true)
+        {
+          ;     
+        }
+     }
+  }
+  
+}
+
+void turn90(int dir)
+{
+  if (dir == 1){  //turn left
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(PWM1, 0);
+    analogWrite(PWM2, 90);
+    delay(700);
+  }
+  else //turn right
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(PWM1, 90);
+    analogWrite(PWM2, 0);
+    delay(700);
+  }
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(PWM1, lMSpeed);
+  analogWrite(PWM2, rMSpeed);
+}
+
+void sensorTest()
+{
+  Serial.print("Left ");
+  Serial.println(analogRead(LEFTSENSOR)-baseLevelL);
+  Serial.print("Right ");
+  Serial.println(analogRead(RIGHTSENSOR)-baseLevelR);
+  Serial.print("Front ");
+  Serial.println(analogRead(FRONTSENSOR)-baseLevelF);
+//    Serial.print("Raw sensor Value : ");
+//    Serial.println(analogRead(FRONTSENSOR));
+//    Serial.print("Normalized value: ");
+//    Serial.println(analogRead(FRONTSENSOR)-baseLevelF);
+    delay(900);
+}
+
+void propControl()
+{
+  //positiveCorrection implies we are too far right, negative implies too far left
+  int currDist2 = analogRead(FRONTSENSOR) - baseLevelF;
+  double correction = 0;
+  double pConstant = 0.02;  //0.02
+  double dConstant = 0.005;
+  int lErr = analogRead(LEFTSENSOR)-baseLevelL;
+  int rErr = analogRead(RIGHTSENSOR)-baseLevelR;
+  int err = lErr - rErr; 
+  double dErr = (err-prevErr)/0.1;
+  correction = (pConstant*(abs(err))) + (dConstant*((dErr)));
+  if (err > 20)
+  {    
+    rMSpeed -= correction;
+    lMSpeed += correction;
+  }
+  else if (err < -60) {
+    lMSpeed -= correction;
+    rMSpeed += correction;
+  } else
+  {
+    rMSpeed += 5;
+    lMSpeed = rMSpeed;
+  }
+  if(currDist2 >700 && lErr > 600){
+    lMSpeed -= correction * 2;
+    rMSpeed += correction * 2;
+  }
+  prevErr = err;
+  //Serial.print("Error: ");
+  //Serial.println(err);
+  //Serial.print("Correction: ");
+  //Serial.println(correction);
+  if (lMSpeed < minlMSpeed) {
+    lMSpeed = minlMSpeed;
+  }
+  if (lMSpeed > maxlMSpeed){
+    lMSpeed = maxlMSpeed;
+  }
+  if (rMSpeed < minrMSpeed) {
+    rMSpeed = minrMSpeed;
+  }
+  if (rMSpeed > maxrMSpeed){
+    rMSpeed = maxrMSpeed;
+  }
+//  Serial.print("Motor speeds:");
+//  Serial.println(lMSpeed);
+//  Serial.println(rMSpeed);
+//  Serial.println();
+//  Serial.println();
+//  Serial.println();
+//  Serial.println();
+}
---
+++ testDrive.ino
@@ -0,0 +1,79 @@
+#define PWM1 3
+#define PWM2 9
+#define IN1 4
+#define IN2 5
+#define IN3 7
+#define IN4 8
+#define FRONTSENSOR A5
+#define LEFTSENSOR 18
+#define RIGHTSENSOR 17
+
+int baseLevelF;
+int baseLevelL;
+int baseLevelR;
+int rMSpeed;
+int lMSpeed;
+int prevErr;
+int maxrMSpeed;
+int maxlMSpeed;
+int minrMSpeed;
+int minlMSpeed;
+
+void setup() {
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(FRONTSENSOR, INPUT);
+  pinMode(LEFTSENSOR,INPUT);
+  pinMode(RIGHTSENSOR,INPUT);
+  baseLevelF = 0;
+  baseLevelR = 0;
+  baseLevelL = 0;
+  rMSpeed = 60;
+  lMSpeed = 60;
+  prevErr = 0;
+  maxrMSpeed = 110;
+  maxlMSpeed = 110;
+  minrMSpeed = 50;
+  minlMSpeed = 50;
+  for (int i = 0; i < 25; i++)
+  {
+    baseLevelF += analogRead(FRONTSENSOR);
+    baseLevelR += analogRead(RIGHTSENSOR);
+    baseLevelL += analogRead(LEFTSENSOR);
+    
+    delay(15 );
+  }
+  baseLevelF /= 25;
+  baseLevelR /= 25;
+  baseLevelL /= 25;
+  Serial.println(baseLevelF);
+}
+
+void drive()
+{
+  //frontCrashTest();
+  propControl();
+  
+  analogWrite(PWM1, lMSpeed);
+  analogWrite(PWM2, rMSpeed);
+  delay(100);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  //sensorTest();
+  //frontCrashTest();
+  drive();
+}
